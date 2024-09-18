dbc97e173696e6e19c5ae9eba629cd520e0fcb3a
Muhammad <muhammad.iqbal@uni-due.de>
2024-09-18 19:35:34 +0200

    update layout

--- CL_en.js
+++ CL_en.js
@@ -3,15 +3,11 @@ document.getElementById("tit1").innerHTML = "Computational Engineer";
 document.getElementById("tit2").innerHTML = "Computational Engineer (123)";
 document.getElementById("tit3").innerHTML = "Computational Engineer (123)";
 document.getElementById("comp1").innerHTML = "XYZ";
-document.getElementById("comp2").innerHTML = "XYZ Gmbh";
-document.getElementById("add1").innerHTML = "ABC 123";
-document.getElementById("add2").innerHTML = "3456, IJK";
-document.getElementById("add3").innerHTML = "Land";
-document.getElementById("cont1").innerHTML = "+49 - 123 456 789";
-document.getElementById("cont2").innerHTML = "xyz@contact.com";
 document.getElementById("person1").innerHTML = "Dear XYZ-Team";
 document.getElementById("source1").innerHTML = "StepStone";
 document.getElementById("skill1").innerHTML = "FEA, CFD";
 document.getElementById("skill2").innerHTML = "SBFEM";
 document.getElementById("skill3").innerHTML = "Ansys, Gmsh";
 document.getElementById("skill4").innerHTML = "ParaView";
+document.getElementById("cont1").innerHTML = "<div>XYZ Gmbh</div><div>ABC 123</div><div>3456, IJK</div><div>Land</div><div>+49 - 123 456 789</div><div>xyz@contact.com</div>";
+
